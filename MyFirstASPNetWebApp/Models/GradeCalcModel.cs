e7855fb5f06edcc0b3c814276ca46f284b0265fe
gregeva <evang0911@gmail.com>
2023-02-08 23:57:09 -0700

    Skill check 5!!

---
+++ MyFirstASPNetWebApp/Models/GradeCalcModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyFirstASPNetWebApp.Models
+{
+    public class GradeCalcModel
+    {
+        [Required]
+        public int Assignments { get; set; }
+        public int GroupProject { get; set; }
+        public int Quiz { get; set; }
+        public int MidtermExam { get; set; }
+        public int FinalExam { get; set; }
+        public int Intex { get; set; }
+    }
+}
